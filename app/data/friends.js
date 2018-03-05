8f6210bcc1b91a3410bbe609cee19f9f9e4bd1af
Zach <z_rdevelopment@outlook.com>
2018-03-05 17:20:27 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Donald Trump',
+		photo: 'https://www.youthareawesome.com/wp-content/uploads/2016/03/trump_3401142b.jpg',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Dr. Phil',
+		photo: 'https://www.dailydot.com/wp-content/uploads/cd0/b6/dr-phil-stare.jpg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Judge Judy',
+		photo: 'https://tvbtn.files.wordpress.com/2015/09/judge-judy.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Jimi Hendrix',
+		photo: 'https://img.discogs.com/SLLPFJ_vMX9ZrhktQjO_nS_-iqg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-110593-1449578430-6807.jpeg.jpg',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
